c2f7c1034e8eb3c2b3eb701d83f0698905ef96ef
Mardon1207 <mardonsultonov8@gmail.com>
2023-04-16 23:11:45 -0700

    3

--- if03.py
+++ if03.py
@@ -8,4 +8,14 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    return
+    if a<b<c and c<b<a:
+        l=b
+    if a<c<b and b<c<a:
+        l=c
+    if b<a<c and c<a<b:
+        l=a
+    return l
+a=int(input())
+b=int(input())
+c=int(input())
+print(main(a,b,c))
