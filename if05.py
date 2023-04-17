8b3cc7a8d700a1a4c80a8a4a534f0fe72576e568
Mardon1207 <mardonsultonov8@gmail.com>
2023-04-16 23:22:06 -0700

    5

--- if05.py
+++ if05.py
@@ -6,4 +6,20 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    a=n//10000
+    b=n//1000%10
+    c=n//100%10
+    d=n//10%10
+    f=n%10
+    max=a
+    if b>max:
+        max=b
+    if c>max:
+        max=c
+    if d>max:
+        max=d
+    if f>max:
+        max=f
+    return max
+n=int(input())
+print(main(n))
